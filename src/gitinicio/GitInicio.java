f2669c5e5fbd9e18e09b18331776c2e4f9a22663
Sol Bartolucci <solbartoluccipais@gmail.com>
2022-03-23 21:03:06 -0300

    Segundo commit

--- src/gitinicio/GitInicio.java
+++ src/gitinicio/GitInicio.java
@@ -5,6 +5,7 @@ public class GitInicio {
 
     public static void main(String[] args) {
         System.out.println("Hola soy Sol Bartolucci");
+        System.out.println("Estos son mis inicios con Git");
     }
     
 }
